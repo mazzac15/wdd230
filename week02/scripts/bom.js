9f42fb27342c6fb6db03a2dff0d36b7505235ddd
Cindy Mazza <maz22004@byui.edu>
2023-11-06 12:37:04 -0500

    bom addition

---
+++ week02/scripts/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        const li = document.createElement('li'); 
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        
+        li.textContent = input.value;
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        
+    } else {
+        input.value = '';
+        input.placeholder = "Please Enter a Chapter";
+    }
+});
