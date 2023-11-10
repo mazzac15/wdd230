92ca96021081446e639b45315049a3e8a060ecd5
Cindy Mazza <maz22004@byui.edu>
2023-11-10 09:03:14 -0500

    week 2 team activity updates

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,21 @@
+const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('nav ul');
+
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('open');
+    hambutton.classList.toggle('open');
+})
+
+const myBtn = document.querySelector('#darkBtn');
+const body = document.body;
+
+myBtn.addEventListener('click', () => {
+    myBtn.classList.toggle('dark');
+    body.classList.toggle('dark-mode')
+
+})
+
 const currentYearElement = document.getElementById('currentYear');
 const currentYear = new Date().getFullYear();
 currentYearElement.textContent = currentYear;
