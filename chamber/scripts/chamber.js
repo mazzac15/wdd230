d36c6e0b2e821be952d5e10f1191bdf3d53b7451
Cindy Mazza <maz22004@byui.edu>
2023-11-14 14:45:22 -0500

    chamber updates week 2

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,13 @@
+const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector('nav ul');
+
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('open');
+    hambutton.classList.toggle('open');
+})
+
+
 const currentYearElement = document.getElementById('currentYear');
 const currentYear = new Date().getFullYear();
 currentYearElement.textContent = currentYear;
