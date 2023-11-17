742816d0d003e48e2d7fd7000c133139955d30af
Cindy Mazza <maz22004@byui.edu>
2023-11-17 14:46:46 -0500

    num visits

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -16,6 +16,19 @@ myBtn.addEventListener('click', () => {
 
 })
 
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+} else {
+    visitsDisplay.textContent = 'Welcome, this is your first visit!';
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
 const currentYearElement = document.getElementById('currentYear');
 const currentYear = new Date().getFullYear();
 currentYearElement.textContent = currentYear;
