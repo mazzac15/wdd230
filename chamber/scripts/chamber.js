c125d3113ab58e4c9e2057363b5041e5acb0d8f0
Cindy Mazza <maz22004@byui.edu>
2023-11-20 11:40:10 -0500

    week03 discover page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -15,3 +15,22 @@ currentYearElement.textContent = currentYear;
 const lastModifiedElement = document.getElementById('lastModified');
 const lastModifiedDate = new Date(document.lastModified);
 lastModifiedElement.textContent = 'Last modified: ' + lastModifiedDate.toDateString();
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (localStorage.getItem('lastVisit')) {
+        const lastVisitDate = new Date(localStorage.getItem('lastVisit'));
+        const timeDifference = new Date() - lastVisitDate;
+        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        if (daysDifference === 0) {
+            document.getElementById('days').innerText = "Back so soon! Awesome!";
+        } else {
+            const message = (daysDifference === 1) ? "day" : "days";
+            document.getElementById('days').innerText = 'You last visited ${daysDifference} ${message} ago.';
+        }
+    } else {
+        document.getElementById('days').innerText = "Welcome! Let us know if you have any questions.";
+    }
+
+    localStorage.setItem('lastVisit', new Date().toISOString());
+});
