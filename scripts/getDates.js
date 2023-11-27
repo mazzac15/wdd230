a6b6800af05f2d4f2b67ad585a4644caf3af1e60
Cindy Mazza <maz22004@byui.edu>
2023-11-26 23:59:31 -0500

    week4 form

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -16,6 +16,55 @@ myBtn.addEventListener('click', () => {
 
 })
 
+const pswd1 = document.querySelector("#password");
+const pswd2 = document.querySelector("#password2");
+const message = document.getElementById("message");
+
+pswd2.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    if (pswd1.value !== pswd2.value) {
+        message.textContent = "❗Passwords DO NOT MATCH!";
+        message.style.visibility = "visible";
+		pswd2.style.backgroundColor = "#fff0f3";
+		pswd2.value = "";
+		pswd2.focus();
+        
+	} else {
+		message.style.visibility = "hidden";
+		pswd2.style.backgroundColor = "#fff";
+		pswd2.style.color = "#000";
+        
+	}
+}
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    const emailInput = document.getElementById('email');
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@byui\.edu$/i;
+    const errorMessage = document.getElementById('emailError')
+
+    if (!emailPattern.test(emailInput.value)) {
+        console.log("Invalid email address");
+        errorMessage.textContent = "Please enter a valid email address with the byui.edu domain";
+        emailInput.value = '';
+        emailInput.focus();
+        event.preventDefault();
+    }
+});
+
+const rangeValue = document.getElementById("rangeValue");
+const range = document.getElementById("rating");
+
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangeValue.innerHTML = range.value;
+}
+
+
+
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
@@ -36,3 +85,4 @@ currentYearElement.textContent = currentYear;
 const lastModifiedElement = document.getElementById('lastModified');
 const lastModifiedDate = new Date(document.lastModified);
 lastModifiedElement.textContent = 'Last modified: ' + lastModifiedDate.toDateString();
+
