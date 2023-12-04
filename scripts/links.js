9484ebec090c7592aedb213a8063d0c93f960fe0
Cindy Mazza <maz22004@byui.edu>
2023-12-03 23:34:56 -0500

    week 5

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,44 @@
 const baseURL = "https://mazzac15.github.io/wdd230/";
-const linksURL = "https://mazzac15.github.io/wdd230/links.json";
+const linksURL = "https://mazzac15.github.io/wdd230/data/links.json";
 
-async function getLinksData() {
+async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.table(data);
-    // displayLinksData(data.links);
+    displayLinks(data.weeks);
 }
 
-getLinksData();
+getLinks();
+
+const displayLinks = (weeks) => {
+    let card = document.querySelector('.card')
+    let title = document.createElement('h3');
+    let learningActivities = document.createElement('div');
+
+    title.textContent = 'Learning Activities';
+    card.appendChild(title);
+    card.appendChild(learningActivities);
+    
+    weeks.forEach((week) => {
+        let weekElement = document.createElement('p');
+       
+        weekElement.textContent = `${week.week}: `;
+
+        week.links.forEach((link) => {
+            const linkElement = document.createElement('a');
+            linkElement.href = `${baseURL}${link.url}`;
+            linkElement.textContent = link.title;
+
+            weekElement.appendChild(linkElement);
+
+            const seperator = document.createTextNode(` | `);
+            weekElement.appendChild(seperator);
+        });
+        
+        learningActivities.appendChild(weekElement);
+  
+        weekElement.removeChild(weekElement.lastChild);  
+        
+        
+        
+    })
+}
